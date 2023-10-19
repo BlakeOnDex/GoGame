5d8976f726357ad63e3aabea24a4d3a934b90636
Blake <108043930+BlakeOnDex@users.noreply.github.com>
2023-10-18 21:39:20 -0400

    Add files via upload

---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	// fmt.Println("Waiting for 4 seconds...")
+	// time.Sleep(time.Second * 4)
+	// fmt.Println("Finished")
+
+	// gay := true
+	// if gay == false {
+	// 	fmt.Println("Exiting program...")
+	// 	os.Exit(0)
+	// }
+	// fmt.Println("Not Exiting program...")
+	Direction := ""
+	Answer := 0
+	Riddle := ""
+	fmt.Println("Welcome to my game!")
+	fmt.Println(" Choose:left or right")
+	fmt.Scanln(&Direction)
+	switch Direction {
+	case "left":
+		fmt.Println("GameOver")
+		fmt.Scanln()
+		os.Exit(0)
+	case "right":
+		fmt.Println("Solve this equation")
+		fmt.Println("120 + 678")
+		fmt.Scanln(&Answer)
+		switch Answer {
+		case 798:
+			fmt.Println("move on to the next level")
+			fmt.Println("left or right")
+			fmt.Scanln(&Direction)
+			switch Direction {
+			case "left":
+				fmt.Println("Solve this riddle")
+				fmt.Println("I’m often created weak so I’m not forgotten. Sometimes I’m reset, if my owner forgets me. What am I?")
+				fmt.Scanln(&Riddle)
+				switch Riddle {
+				case "password":
+					
+				    fmt.Println("Exiting program...")
+					fmt.Scanln()
+					os.Exit(0)
+					
+				}
+
+			case "right":
+				fmt.Println("GameOver")
+				fmt.Scanln()
+				os.Exit(0)
+			}
+		default:
+			fmt.Println("GameOver")
+			fmt.Scanln()
+			os.Exit(0)
+		}
+
+	}
+
+}
